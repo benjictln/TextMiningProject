33292b157de2c0bbd703c17adb129f1b5786f005
Benjamin Castellan <benjamincastellan@gmail.com>
2018-11-23 22:38:16 +0900

    get informations about the sections and the nb of articles"

--- issue_trend/test.py
+++ issue_trend/test.py
@@ -6,6 +6,10 @@ path_data = "../data/"
 names_file_data = []
 keys = ["title", " author", " time", " description", " body", " section"]
 years = ["2015", "2016", "2017"]
+sections = ["Defense", "", "Politics", "Social affairs", "North Korea",
+            "International", "National", "Science", "Foreign  Affairs",
+            "Education", "Foreign Policy", "사용안함 - Diplomatic Circuit",
+            "사용안함 - Environment", "사용안함 - Sharing", "Diplomatic Circuit"]
 
 for i in range(8):
     names_file_data.append(path_data + "koreaherald_1517_" + str(i) + ".json")
@@ -47,6 +51,7 @@ def test2():
             pp.pprint(r.get_ranked_phrases_with_scores()[:2])  # To get keyword phrases ranked highest to lowest.
 
 
+# function useful only to get some datas at the beginning
 def nb_of_articles_per_year():
     '''
     This function goes through all the articles and print the number of articles in each year
@@ -70,6 +75,40 @@ def nb_of_articles_per_year():
                     nb_in_years.append(1)
     for k in range(len(years)):
         print("The year is " + years[k] + " and the number of article is " + str(nb_in_years[k]) + "\n")
+
+
+# function useful only to get some datas at the beginning
+def getTheSectionTypeAndNmOfArticlesInsidePerYear():
+
+    sections = []
+    nb_in_sections_per_year = [[] for _ in range(len(years))]  # nb_in_sections_per_year[i] represents years[i]
+
+    for index_year in range(len(years)):
+        for i in range(len(names_file_data)):
+            with open(names_file_data[i], 'r') as f:
+                data = json.load(f)
+                nb_docs = len(data.get(keys[0]))
+                for j in range(nb_docs):
+                    date_article = data.get(keys[2]).get(str(j))
+                    if isArticleInThisYear(date_article,years[index_year]):
+                        section = data.get(keys[5]).get(str(j))
+                        section_already_seen = False
+                        for index_section in range(len(sections)):
+                            if section == sections[index_section]:
+                                section_already_seen = True
+                                nb_in_sections_per_year[index_year][index_section] += 1
+                        if not section_already_seen:
+                            for index_year_2 in range(len(years)):
+                                if index_year == index_year_2:
+                                    nb_in_sections_per_year[index_year_2].append(1)
+                                else:
+                                    nb_in_sections_per_year[index_year_2].append(0)
+                            sections.append(section)
+
+    for index_year in range(len(years)):
+        print("FOR YEAR: " +years[index_year])
+        for k in range(len(sections)):
+            print("\tfor section:\t" + sections[k] + "\t there was \t" + str(nb_in_sections_per_year[index_year][k]) + " articles.")
 
 
 def isArticleInThisYear(date_article, targeted_year):
@@ -114,15 +153,9 @@ def test3(argv):
                             section = data.get(keys[5]).get(str(j))
                             print(section)
 
-# min_length=2, max_length=4
-#r = Rake(ranking_metric=Metric.DEGREE_TO_FREQUENCY_RATIO)
-#
-#
 
-#test3()
 
 arg = 1
-
 while (arg!=0):
     arg = int(input("Please enter a number (0 to exit): \n"))
     if arg == 1:
